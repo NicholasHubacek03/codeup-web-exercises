b01badcd99aa10007f7be7bb199fa6068d306309
Nicholas Hubacek <nicholashubacek@Nicholass-Air.localdomain>
2023-02-17 12:24:21 -0600

    js with html lecture note's

---
+++ js/external.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("This is my external js file");
+let quantity = 3;
+console.log(quantity);
+
+let myAge = 25;
+console.log(myAge);
+
+//alert("Hello welcome to my website");
+
+let submit = confirm(" Are you Sure?");
+console.log(submit)
+
+let age = prompt("What is your age?")
+alert(" you're! "  +  age )
